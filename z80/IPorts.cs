0f92e795067a4a736203f3aebaa466cdd8fe8a1d
Foo S Bar <horseflyrider@gmail.com>
2026-08-07 15:07:36 -0700

    Save and load the port settings in the asmprg file. default to tiny basic ports.

--- z80/IPorts.cs
+++ z80/IPorts.cs
@@ -2,11 +2,11 @@ namespace z80
 {
     public interface IPorts
     {
+        string Name{ get; }
         byte ReadPort(ushort address);
         void WritePort(ushort address, byte value);
         bool NMI { get; }
         bool MI { get; }
         byte Data { get; }
-
     }
 }
